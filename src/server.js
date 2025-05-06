9c4dca1713765272ba5887337d911a3ed8961dcd
Володимир <167300466+hhvolodymyrhh@users.noreply.github.com>
2025-05-06 20:58:50 +0300

    routs

--- src/server.js
+++ src/server.js
@@ -5,7 +5,9 @@ import cors from 'cors';
 
 
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import studentsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -25,47 +27,17 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data: contacts,
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+  app.use(studentsRouter);
 
-    if (!contact) {
-      res.status(404).json({
-        message: "contact not found"
-      });
-      return;
-    }
-    res.status(200).json({
-      status: 200,
-	    message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
+  app.use('*', notFoundHandler);
 
-
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
