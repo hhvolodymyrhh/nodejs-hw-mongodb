ed643e949dc56662baf1bf550f75cc88fe4932e4
Володимир <167300466+hhvolodymyrhh@users.noreply.github.com>
2025-05-16 16:07:37 +0300

    fix {userId}

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -14,7 +14,7 @@ export const getAllContacts = async ({
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.find(userId);
+  const contactsQuery = ContactsCollection.find({ userId });
 
    if (filter.type) {
     contactsQuery.where('contactType').equals(filter.type);
