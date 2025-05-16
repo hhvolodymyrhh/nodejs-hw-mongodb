703655842d8a263a7b6deb22a123adf5717002ef
Володимир <167300466+hhvolodymyrhh@users.noreply.github.com>
2025-05-16 15:22:26 +0300

    post contact

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -47,7 +47,7 @@ export const getContactByIdController = async (req, res) => {
 
 export const createContactController = async (req, res) => {
     const userId = req.user._id;
-     const contact = await createContact(req.body, userId);
+     const contact = await createContact( userId, req.body);
 
     res.status(201).json({
     status: 201,
