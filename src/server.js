605608bb3d15a56dd949a2331fdfdd2cf977f7f0
Володимир <167300466+hhvolodymyrhh@users.noreply.github.com>
2025-05-31 20:23:39 +0300

    put patch delete

--- src/server.js
+++ src/server.js
@@ -18,7 +18,6 @@ const PORT = Number(getEnvVar('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
 
-
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
@@ -37,6 +36,9 @@ export const setupServer = () => {
     });
   });
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   app.use(router);
 
   app.use(notFoundHandler);
@@ -47,8 +49,7 @@ export const setupServer = () => {
     console.log(`Server is running on port ${PORT}`);
   });
 
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
+
 };
 
 
